86b658274f408ba42ff362fd9a32344f7c6104b9
fanghuayong <fang7506@yeah.net>
2019-11-10 22:46:16 +0800

    第一次提交

---
+++ notes.js
@@ -0,0 +1,295 @@
+var notes = [{
+    name: "JavaScript",
+    list: [{
+        listName: "事件基础",
+        listUrl: "https://zhuanlan.zhihu.com/p/84679512",
+    },{
+        listName: "对象单个中的属性设置",
+        listUrl: "https://zhuanlan.zhihu.com/p/80126154",
+    },{
+        listName: "对象中整体的属性设置及克隆",
+        listUrl: "https://zhuanlan.zhihu.com/p/84570654",
+    },{
+        listName: "函数的三大特性",
+        listUrl: "https://zhuanlan.zhihu.com/p/84596455",
+    },{
+        listName: "函数bind",
+        listUrl: "https://zhuanlan.zhihu.com/p/84612721",
+    },{
+        listName: "函数的call、apply",
+        listUrl: "https://zhuanlan.zhihu.com/p/76002842",
+    },{
+        listName: "原型、原型链、继承",
+        listUrl: "https://zhuanlan.zhihu.com/p/83691879",
+    },{
+        listName: "闭包",
+        listUrl: "https://zhuanlan.zhihu.com/p/79692367",
+    },{
+        listName: "数组",
+        listUrl: "https://zhuanlan.zhihu.com/p/85352115",
+    },{
+        listName: "字符串",
+        listUrl: "https://zhuanlan.zhihu.com/p/85379843",
+    },{
+        listName: "正则表达式",
+        listUrl: "https://zhuanlan.zhihu.com/p/85396216",
+    },{
+        listName: "正则表达式练习",
+        listUrl: "https://zhuanlan.zhihu.com/p/85441850",
+    },]
+},{
+    name: "HTML5",
+    list: [{
+        listName: "Geolocation地理位置",
+        listUrl: "https://zhuanlan.zhihu.com/p/84309280",
+    },{
+        listName: "Worker多线程处理",
+        listUrl: "https://zhuanlan.zhihu.com/p/84344665",
+    },]
+},{
+    name: "CSS3",
+    list: [{
+        listName: "常用选择器",
+        listUrl: "https://zhuanlan.zhihu.com/p/84371569",
+    },{
+        listName: "常用属性",
+        listUrl: "https://zhuanlan.zhihu.com/p/84389099",
+    },{
+        listName: "盒模型",
+        listUrl: "https://zhuanlan.zhihu.com/p/84456259",
+    },{
+        listName: "Columns布局",
+        listUrl: "https://zhuanlan.zhihu.com/p/84474948",
+    },{
+        listName: "Flex弹性盒子",
+        listUrl: "https://zhuanlan.zhihu.com/p/84498308",
+    },{
+        listName: "Media媒体查询",
+        listUrl: "https://zhuanlan.zhihu.com/p/84512126",
+    },{
+        listName: "Transform和Transition",
+        listUrl: "https://zhuanlan.zhihu.com/p/85457402",
+    },{
+        listName: "移动端布局",
+        listUrl: "https://zhuanlan.zhihu.com/p/84528418",
+    },]
+},{
+    name: "网络",
+    list: [{
+        listName: "ajax手动封装",
+        listUrl: "https://zhuanlan.zhihu.com/p/79398243",
+    },{
+        listName: "http协议",
+        listUrl: "https://zhuanlan.zhihu.com/p/84795632",
+    },{
+        listName: "JSONP跨域",
+        listUrl: "https://zhuanlan.zhihu.com/p/84989381",
+    },{
+        listName: "Cookie使用机制",
+        listUrl: "https://zhuanlan.zhihu.com/p/85224083",
+    },{
+        listName: "WebStorage本地存储",
+        listUrl: "https://zhuanlan.zhihu.com/p/85232575",
+    },{
+        listName: "CORS跨域资源共享",
+        listUrl: "https://zhuanlan.zhihu.com/p/85264225",
+    },]
+},{
+    name: "jQuery",
+    list: [{
+        listName: "引用dom",
+        listUrl: "https://zhuanlan.zhihu.com/p/85484836",
+    },{
+        listName: "jQuery整体构架封装",
+        listUrl: "https://zhuanlan.zhihu.com/p/74170805",
+    },{
+        listName: "属性-文本",
+        listUrl: "https://zhuanlan.zhihu.com/p/86321769",
+    },{
+        listName: "属性-特性:attr()和prop()",
+        listUrl: "https://zhuanlan.zhihu.com/p/74297604",
+    },{
+        listName: "属性-class",
+        listUrl: "https://zhuanlan.zhihu.com/p/86338924",
+    },{
+        listName: "css方法",
+        listUrl: "https://zhuanlan.zhihu.com/p/85914423",
+    },{
+        listName: "dom的筛选",
+        listUrl: "https://zhuanlan.zhihu.com/p/86370895",
+    },{
+        listName: "dom的查询",
+        listUrl: "https://zhuanlan.zhihu.com/p/86404129",
+    },{
+        listName: "dom的插入",
+        listUrl: "https://zhuanlan.zhihu.com/p/86434460",
+    },{
+        listName: "dom的删除",
+        listUrl: "https://zhuanlan.zhihu.com/p/86452191",
+    },{
+        listName: "事件-鼠标",
+        listUrl: "https://zhuanlan.zhihu.com/p/86475960",
+    },{
+        listName: "事件-焦点",
+        listUrl: "https://zhuanlan.zhihu.com/p/86511882",
+    },{
+        listName: "事件-委托",
+        listUrl: "https://zhuanlan.zhihu.com/p/86514695",
+    },{
+        listName: "动画:隐藏显示",
+        listUrl: "https://zhuanlan.zhihu.com/p/86625498",
+    },{
+        listName: "动画:自定义",
+        listUrl: "https://zhuanlan.zhihu.com/p/86633809",
+    },{
+        listName: "工具方法",
+        listUrl: "https://zhuanlan.zhihu.com/p/86707181",
+    },{
+        listName: "extend",
+        listUrl: "https://zhuanlan.zhihu.com/p/86718886",
+    }]
+},{
+    name: "ES6",
+    list: [{
+        listName: "let和const",
+        listUrl: "https://zhuanlan.zhihu.com/p/74782005",
+    },{
+        listName: "字符串扩展",
+        listUrl: "https://zhuanlan.zhihu.com/p/74852553",
+    },{
+        listName: "解构赋值",
+        listUrl: "https://zhuanlan.zhihu.com/p/74987363",
+    },{
+        listName: "参数处理",
+        listUrl: "https://zhuanlan.zhihu.com/p/75149169",
+    },{
+        listName: "数字的操作",
+        listUrl: "https://zhuanlan.zhihu.com/p/75198019",
+    },{
+        listName: "箭头函数",
+        listUrl: "https://zhuanlan.zhihu.com/p/75462918",
+    },{
+        listName: "对象的扩展",
+        listUrl: "https://zhuanlan.zhihu.com/p/75860614",
+    },{
+        listName: "类的基础使用语法",
+        listUrl: "https://zhuanlan.zhihu.com/p/75910572",
+    },{
+        listName: "类的继承",
+        listUrl: "https://zhuanlan.zhihu.com/p/75977514",
+    },{
+        listName: "数组的扩展",
+        listUrl: "https://zhuanlan.zhihu.com/p/76232946",
+    },{
+        listName: "迭代器iterator",
+        listUrl: "https://zhuanlan.zhihu.com/p/76383309",
+    },{
+        listName: "生成器Generator",
+        listUrl: "https://zhuanlan.zhihu.com/p/76451686",
+    },{
+        listName: "异步Promise",
+        listUrl: "https://zhuanlan.zhihu.com/p/76551947",
+    },]
+},{
+    name: "Webpack",
+    list: [{
+        listName: "webpack命令的使用",
+        listUrl: "https://zhuanlan.zhihu.com/p/77302774",
+    },{
+        listName: "四个核心概念",
+        listUrl: "https://zhuanlan.zhihu.com/p/77519314",
+    },{
+        listName: "webpack初探与模块化",
+        listUrl: "https://zhuanlan.zhihu.com/p/79058272",
+    },]
+},{
+    name: "Sass",
+    list: [{
+        listName: "sass嵌套",
+        listUrl: "https://zhuanlan.zhihu.com/p/79144975",
+    },{
+        listName: "变量与Mixin",
+        listUrl: "https://zhuanlan.zhihu.com/p/79226769",
+    },{
+        listName: "函数",
+        listUrl: "https://zhuanlan.zhihu.com/p/79319073",
+    },]
+},{
+    name: "Vue",
+    list: [{
+        listName: "初始vue",
+        listUrl: "https://zhuanlan.zhihu.com/p/79423596",
+    },{
+        listName: "html模板",
+        listUrl: "https://zhuanlan.zhihu.com/p/79439055",
+    },{
+        listName: "vue-cli 3.0创建",
+        listUrl: "https://zhuanlan.zhihu.com/p/79561192",
+    },{
+        listName: "定义组件template",
+        listUrl: "https://zhuanlan.zhihu.com/p/79583985",
+    },{
+        listName: "事件绑定v-on",
+        listUrl: "https://zhuanlan.zhihu.com/p/79600591",
+    },{
+        listName: "双向数据绑定 计算属性与监听",
+        listUrl: "https://zhuanlan.zhihu.com/p/79641309",
+    },{
+        listName: "动态添加样式",
+        listUrl: "https://zhuanlan.zhihu.com/p/79772343",
+    },{
+        listName: "条件渲染v-if/v-for",
+        listUrl: "https://zhuanlan.zhihu.com/p/79916025",
+    },{
+        listName: "深入及生命周期",
+        listUrl: "https://zhuanlan.zhihu.com/p/80043173",
+    },{
+        listName: "单文件组件 render函数",
+        listUrl: "https://zhuanlan.zhihu.com/p/80466193",
+    },{
+        listName: "组件实例 局部样式",
+        listUrl: "https://zhuanlan.zhihu.com/p/80654285",
+    },{
+        listName: "组件间传值",
+        listUrl: "https://zhuanlan.zhihu.com/p/80913683",
+    },{
+        listName: "slot内容分发 动态组件",
+        listUrl: "https://zhuanlan.zhihu.com/p/81071091",
+    },{
+        listName: "常见的form表单 自定义v-mode",
+        listUrl: "https://zhuanlan.zhihu.com/p/81225595",
+    },{
+        listName: "自定义指令",
+        listUrl: "https://zhuanlan.zhihu.com/p/81246032",
+    },{
+        listName: "Filter与Mixins",
+        listUrl: "https://zhuanlan.zhihu.com/p/81384332",
+    },{
+        listName: "动画的应用css",
+        listUrl: "https://zhuanlan.zhihu.com/p/81447413",
+    },{
+        listName: "动画的应用js",
+        listUrl: "https://zhuanlan.zhihu.com/p/81488955",
+    },{
+        listName: "router的基础",
+        listUrl: "https://zhuanlan.zhihu.com/p/82064389",
+    },{
+        listName: "router-link动态路由",
+        listUrl: "https://zhuanlan.zhihu.com/p/82179329",
+    },{
+        listName: "router零散知识点",
+        listUrl: "https://zhuanlan.zhihu.com/p/82296509",
+    },{
+        listName: "vuex基础",
+        listUrl: "https://zhuanlan.zhihu.com/p/82367314",
+    },{
+        listName: "vuex getters mutations actions",
+        listUrl: "https://zhuanlan.zhihu.com/p/82411620",
+    },]
+},{
+    name: "Git",
+    list: [{
+        listName: "git基础",
+        listUrl: "https://zhuanlan.zhihu.com/p/82592178",
+    },]
+},]
